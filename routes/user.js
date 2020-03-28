5d17ff641316bf3b0ad0bc18c50dd9dfeafc97fe
Defcon27 <defcon.sentinal95@gmail.com>
2020-03-28 14:01:03 +0530

    login error handling done

--- routes/user.js
+++ routes/user.js
@@ -7,26 +7,37 @@ router.get("/", function (req, res, next) {
     res.sendFile(__dirname + "/login.html");
 });
 
+router.get("/loginerror", function (req, res, next) {
+    res.sendFile(__dirname + "/loginerror.html");
+});
+
 
 router.post("/", function (req, res, next) {
     let id = req.body.userid;
     let pass = req.body.password;
     console.log(pass);
-    var docs = null
+
+
 
     User.findOne({ userID: "9999" }, function (err, docs) {
+        var loginerror = true;
         if (err) {
             console.log(err);
+            loginerror = true;
         }
-        else if (docs != null) {
+        else {
+            loginerror = false;
             if (pass == docs.password) {
                 console.log("Login Success");
-                res.redirect("/home")
+                res.redirect("/home");
+            } else {
+                res.redirect("/loginerror");
             }
         }
-        console.log("Wrong input");
 
     });
+
+
 
 });
 
