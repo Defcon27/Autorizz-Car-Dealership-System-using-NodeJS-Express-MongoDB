46414c4b2e8b6140b5c3a0b3e32e925c8c7ed6ee
Defcon27 <defcon.sentinal95@gmail.com>
2020-03-29 23:24:50 +0530

    service module done

---
+++ models/servicing.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var servicingSchema = new Schema({
+    customerImage: { type: String, required: true },
+    customerName: { type: String, required: true },
+    customerEmail: { type: String, required: true },
+    customerPhone: { type: String, required: true },
+    customerModel: { type: String, required: true },
+    customerType: { type: String, required: true },
+    customerDone: { type: String, required: true },
+    customerBill: { type: String, required: true },
+    Date: { type: String, required: true }
+});
+
+module.exports = mongoose.model("servicing", servicingSchema, "servicing");
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 router.use(express.static("public"));
 var Model = require("../models/model");
+var Servicing = require("../models/servicing");
+var nodemailer = require('nodemailer');
 
 router.get("/shop", function (req, res, next) {
   Model.find(function (err, docs) {
@@ -11,8 +13,8 @@ router.get("/shop", function (req, res, next) {
 });
 
 router.get("/service", function (req, res, next) {
-  Model.find(function (err, docs) {
-    res.render("shop/index.hbs", { title: "Autorizz", models: docs });
+  Servicing.find(function (err, docs) {
+    res.render("servicing/servicing.hbs", { title: "Autorizz", servicecars: docs });
     console.log(docs);
   });
 });
@@ -29,18 +31,48 @@ router.get("/booknow/:id", function (req, res, next) {
     }
     else {
       console.log(doc);
-
       res.render("shop/booking", { title: "Autorizz", model: doc })
     }
 
   });
 });
 
-
 /* GET home page. */
 router.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+
+
+router.get("/service/email", function (req, res) {
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'defcon.sentinal95@gmail.com',
+      pass: 'hemanth2000'
+    }
+  });
+
+  var mailOptions = {
+    from: 'defcon.sentinal95@gmail.com',
+    to: 'hemanthkollipara95@gmail.com',
+    subject: 'Autorizz Garage',
+    text: 'Dear Customer servicing for vehicle has been done. Please drive back your beloved vehicle.'
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+
+  });
+
+});
+
+
 
 
 module.exports = router;
