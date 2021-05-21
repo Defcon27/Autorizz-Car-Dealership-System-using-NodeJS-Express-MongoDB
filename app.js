b8bdcbdac6d8f08941c2920a05c29fdd5d266989
Defcon27 <defcon.sentinal95@gmail.com>
2021-05-22 00:15:21 +0530

    Added admin login

--- app.js
+++ app.js
@@ -1,38 +1,84 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var expressHbs = require("express-handlebars");
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/autorizz", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Database Connection acquired");
-});
+const electricRouter = require('./routes/electric_index');
+const gasRouter = require('./routes/gas_index');
+const adminRouter = require('./routes/admin');
+var UserModel = require("./models/CustomerModel");
+const app = express();
 
-var indexRouter = require("./routes/index");
-var userRoute = require("./routes/user")
-var app = express();
+
+//Connecting to Mongodb
+const db = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb://localhost:27017/autorizz', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+
+    console.log("MongoDB connected");
+
+  } catch (err) {
+    console.log("MongoDB Error : Failed to connect");
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+db();
+
 
 // view engine setup
-//app.set("views", path.join(__dirname, "views"));
-app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
-app.set("view engine", ".hbs");
+app.engine('.hbs', exphbs({
+  defaultLayout: 'layout', extname: '.hbs',
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+  }
+}));
+app.set('view engine', '.hbs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 
-app.use("/home", indexRouter);
-app.use("/", userRoute);
+console.log("App running on Localhost:5000");
+
+
+// Routing
+app.get('/', (req, res) => {
+  res.redirect('/electric');
+});
+
+app.use('/admin', adminRouter);
+app.use('/electric', electricRouter);
+app.use('/gas', gasRouter);
+
+
+//Users
+app.post('/customer', async (req, res) => {
+
+  const user = new UserModel({
+    name: req.body.username,
+    email: req.body.useremail,
+    phone: req.body.userphone
+  })
+
+  const user_res = await user.save();
+  console.log(user_res);
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -43,11 +89,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
